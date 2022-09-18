14378534c862d92ef75b8650ee8dbb7bc5d6610f
Olesia Libra <98489034+Solymar1388@users.noreply.github.com>
2022-09-18 15:10:02 +0300

    added Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">dictionary
-      </header>
+      <div className="container">
+        <header className="App-header">dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center App-footer">
+          <small>Coded by Olesia Libra</small>
+        </footer>
+      </div>
     </div>
   );
 }
